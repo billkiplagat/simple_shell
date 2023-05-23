7d17b1207a2048a6485dec4dc982d9828a117c4f
billkiplagat <bill.kiplagat.7@gmail.com>
2023-05-23 16:52:16 +0300

    changes on main.h and shell_stdlib.c files

--- shell_stdlib.c
+++ shell_stdlib.c
@@ -1,11 +1,101 @@
 #include "main.h"
 
 /**
- * get_sigint - Handle the crtl + c call in prompt
- * @sig: Signal handler
- */
-void get_sigint(int sig)
+* get_len - Get the lenght of a number.
+* @n: type int number.
+* Return: Lenght of a number.
+*/
+int get_len(int n)
 {
-(void)sig;
-write(STDOUT_FILENO, "\n($) ", 5);
+unsigned int n1;
+int len = 1;
+
+if (n < 0)
+{
+len++;
+n1 = n * -1;
+}
+else
+{
+n1 = n;
+}
+while (n1 > 9)
+{
+len++;
+n1 = n1 / 10;
+}
+
+return (len);
+}
+/**
+* _itoa - function converts int to string.
+* @n: type int number
+* Return: String.
+*/
+char *_itoa(int n)
+{
+unsigned int n1;
+int len = get_len(n);
+char *buf;
+
+buf = malloc(sizeof(char) * (len + 1));
+if (buf == 0)
+return (NULL);
+
+*(buf + len) = '\0';
+
+if (n < 0)
+{
+n1 = n * -1;
+buf[0] = '-';
+}
+else
+{
+n1 = n;
+}
+
+len--;
+do {
+*(buf + len) = (n1 % 10) + '0';
+n1 = n1 / 10;
+len--;
+}
+while (n1 > 0)
+;
+return (buf);
+}
+
+/**
+* _atoi - converts a string to an integer.
+* @s: input string.
+* Return: integer.
+*/
+
+int _atoi(char *s)
+{
+unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+while (*(s + count) != '\0')
+{
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+break;
+
+if (*(s + count) == '-')
+pn *= -1;
+
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+{
+if (size > 0)
+m *= 10;
+size++;
+}
+count++;
+}
+
+for (i = count - size; i < count; i++)
+{
+oi = oi + ((*(s + i) - 48) * m);
+m /= 10;
+}
+return (oi * pn);
 }
